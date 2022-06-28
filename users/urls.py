cebca092da8fc305860b92ecd8d8d9ff75061707
고현영 <gusdud3890@gmail.com>
2022-06-28 14:41:06 +0900

    ADD: quiz result post and get

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,12 @@
 
-from django.urls import path,include
+from django.urls import path
 
-from users.views import UserProfile,UserCreate
+from users.views import UserProfile, UserCreate, UserRankView
 
 urlpatterns = [
     path('login/', UserCreate.as_view(), name='user_create'),
-    path('profile/', UserProfile.as_view(), name = 'user_profile')
+    path('profile/', UserProfile.as_view(), name = 'user_profile'),
+    path('rank/', UserRankView.as_view(), name = 'user-rank'),
 ] 
 
 
--- users/views.py
+++ users/views.py
@@ -1,12 +1,13 @@
 import requests
 
-from rest_framework import generics, status
-from rest_framework.response import Response
+from rest_framework                  import generics, status
+from rest_framework.response         import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 
-from users.models import User,UserRank
-from users.serializers import UserProfileSerializer, UserCreateSerialiser
-from cores.decorator import login_authorization
+from users.models      import User,UserRank
+from users.serializers import UserProfileSerializer, UserCreateSerialiser, UserRankSerializer
+from cores.decorator   import login_authorization
+
 
 class UserCreate(generics.GenericAPIView):
     queryset = User.objects.all()
@@ -45,6 +46,7 @@ class UserCreate(generics.GenericAPIView):
         response.set_cookie("refresh_token", user.refresh, httponly=True)
         return response
 
+
 class UserProfile(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserProfileSerializer
@@ -55,3 +57,22 @@ class UserProfile(generics.ListAPIView):
         queryset = User.objects.filter(id=user)
         serializer = UserProfileSerializer(queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class UserRankView(generics.ListCreateAPIView):
+
+    @login_authorization
+    def create(self, request):
+        serializer = UserRankSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save(user=request.user.id)
+            return Response(serializer.data)
+        return Response(serializer.errors)
+
+    @login_authorization
+    def list(self, request):
+        user       = request.user.id
+        user_rank  = UserRank.objects.get(user=user)
+        serializer = UserRankSerializer(user_rank)
+        return Response(serializer.data)
