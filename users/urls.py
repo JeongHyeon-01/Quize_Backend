7b52a0013838e99aff6d85a115ff82b3efd087bc
Jeonghyeon <89664413+JeongHyeon-01@users.noreply.github.com>
2022-07-13 06:53:25 +0900

    fix

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,7 @@ from users.views import UserProfile, UserCreate, UserRankView,GoogleUserlogin,Go
 urlpatterns = [
     path('kakao/login/', KakaoLogin.as_view(),name='kakao_login'), # 혹시모를 카카오 로그인
     path('google/', GoogleUserlogin.as_view(), name='google_redirect'), #원빈님 팀 에서 사용하듯이 한다면 살릴걸
-    path('google/callback', GoogleUserCreate.as_view(), name = 'google_user'), #원빈님 팀 에서 사용하듯이 한다면 살릴걸
+    path('google/callback/', GoogleUserCreate.as_view(), name = 'google_user'), #원빈님 팀 에서 사용하듯이 한다면 살릴걸
     path('login/', UserCreate.as_view(), name='user_create'), # 현재 메인
     path('profile/', UserProfile.as_view(), name = 'user_profile'),
     path('rank/', UserRankView.as_view(), name = 'user-rank'),
