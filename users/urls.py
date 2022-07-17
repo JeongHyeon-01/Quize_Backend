d84ec65ade6108b73f2c9a9cb87f1672b782a2e3
Jeonghyeon <89664413+JeongHyeon-01@users.noreply.github.com>
2022-07-17 23:18:01 +0900

    [DELETE]Regacy code

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path
 
-from users.views import UserProfile, UserCreate, UserRankView,GoogleUserlogin,GoogleUserCreate, KakaoLogin
+from users.views import UserProfile, UserCreate, UserRankView
 urlpatterns = [
-    path('kakao/login/', KakaoLogin.as_view(),name='kakao_login'), # 혹시모를 카카오 로그인
-    path('google/', GoogleUserlogin.as_view(), name='google_redirect'), #원빈님 팀 에서 사용하듯이 한다면 살릴걸
-    path('google/callback/', GoogleUserCreate.as_view(), name = 'google_user'), #원빈님 팀 에서 사용하듯이 한다면 살릴걸
-    path('login/', UserCreate.as_view(), name='user_create'), # 현재 메인
+    path('login/', UserCreate.as_view(), name='user_create'),
     path('profile/', UserProfile.as_view(), name = 'user_profile'),
     path('rank/', UserRankView.as_view(), name = 'user-rank'),
 ] 
